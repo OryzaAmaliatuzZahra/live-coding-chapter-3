3196e2cec95f220cf205ea9f487bc0b90f21288e
OryzaAZ <zoryzaamaliatuz@gmail.com>
2023-03-27 14:16:51 +0700

    live coding chapter 3 commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// console.log("hello")
+// // alert merupakan code untuk bagian frontend sehingga tidak akan bisa dirun di terminal/bagian backend
+// alert('Hello FSW3');
+// function hello() {
+//     console.log('hello world')
+// }
+
+// ini untuk ngeselect/document bagian mana yang akan diselect
+document.querySelector("h1").innerHTML = "Good Bye";
